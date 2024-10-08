d433f802e7cca63e3ee75c130cb74c1ac05419da
Your Name <you@example.com>
2024-10-08 21:55:26 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Я – JavaScript!");
+let name = "Іван";
+let admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Земля";
+let currentUserName = "Левко";
+
+const BIRTHDAY = '17.05.2008'; 
+const age = someCode(BIRTHDAY); 
