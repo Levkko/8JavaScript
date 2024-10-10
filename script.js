f2e473b8ce27788bca4830e149dc547734c13c0f
Your Name <you@example.com>
2024-10-10 17:01:27 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,38 +1,8 @@
-let a = 1, b = 1;
-let c = ++a; 
-let d = b++; 
+5 > 4 //true
+"ананас" > "яблуко" //false
+"2" > "12" //true
+undefined == null //
+undefined === null //
+null == "\n0\n" //
+null === +"\n0\n" //
 
-//a = 2
-//b = 2
-//c = 2
-//d = 1
-
-
-let a = 2;
-let x = 1 + (a *= 2);
-
-//a = 4
-//x = 5
-
-
-"" + 1 + 0 // 10
-"" - 1 + 0 // -1
-true + false // 1
-6 / "3" // 2
-"2" * "3" // 6
-4 + 5 + "px" // 9px
-"$" + 4 + 5 // $45
-"4" - 2 // 2
-"4px" - 2 // NaN
-"  -9  " + 5 // -9 5
-"  -9  " - 5 // -14
-null + 1 // 1
-undefined + 1 // NaN
-" \t \n" - 2 // -2
-
-
-
-let a = prompt("1", 1);
-let b = prompt("2", 2);
-
-alert(a + b); // 12
