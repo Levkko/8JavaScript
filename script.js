fdb22667fae080aa8f0902103140a6fbf70b1b91
Your Name <you@example.com>
2024-10-12 15:03:14 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
+alert( null || 2 || undefined );
+// 2, бо це перше правдиве значення
 
-//if (рядок з нулем)
-if ("0") { 
-    alert( 'Привіт' );
-  } //Так, alert спрацює в цьому випадку
+alert( alert(1) || 2 || alert(3) );
+//сочатку 1, потім 2
+
+alert( 1 && null && 2 );
+//null
+
+alert( alert(1) && alert(2) );
+//1, а потім undefined
+
+alert( null || 2 && 3 || 4 );
+//3
+
+if (!(age >= 14 && age <= 90));
+if (age < 14 || age > 90);
+
+if (-1 || 0) alert( 'перший' );
+if (-1 && 0) alert( 'другий' );
+if (null || -1 && 1) alert( 'третій' );
+//перший і третій
 
 
-//Назва JavaScript
-let company = prompt("Яка офіційна назва JavaScript?");
-
-    if (company === "ECMAScript") {
-        alert("Правильно!");
+let registration = prompt("Введіть логін");
+    if (registration === "admin") {
+        let password = prompt("Введіть пароль");
+            if (password === "host") {
+                alert("Ласкаво просимо!");
+            } else if (password === '' || password === null) {
+                alert("Скасовано");
+            } else {
+                alert("Невірний пароль");
+            }
+    } else if (registration === '' || registration === null) {
+        alert("Скасовано");
     } else {
-        alert("Ви не знаєте? ECMAScript!");
+        alert("Я вас не знаю!");
     }
-
-
-//Покажіть знак
-let number = Number(prompt("Введіть будь-яке число"));
-    if (number > 0) {
-        alert(1)
-    } else if (number < 0) {
-        alert(-1)
-    } else {
-        alert(0)
-    }
-
-
-// Перепишіть 'if' на '?'
-let result;
-    result = (a + b < 4) ? 'Нижче' : 'Вище';
-
-
-//Перепишіть 'if..else' на '?'
-let message =
-    (login === 'Працівник') ? 'Привіт':
-    (login === 'Директор') ? 'Вітаю':
-    (login === '') ? 'Немає логіну':
-    '';
