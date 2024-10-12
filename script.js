fcfc0bfb8d75d779b2f743f50518d8db36ea5ef1
Your Name <you@example.com>
2024-10-12 20:38:51 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,63 +1,31 @@
-//Останнє значення циклу
-let i = 3;
-while (i) {
-  alert( i-- );
-}
-//1. Тому що 0 це false і скрипт обривається
-
-
-//Яке значення виведе цикл "while"?
-let i = 0;
-while (++i < 5) alert( i );//1-4
-let i = 0;
-while (i++ < 5) alert( i );//1-5
-
-
-//Яке значення виведе цикл "for"?
-for (let i = 0; i < 5; i++) alert( i );//1-4
-for (let i = 0; i < 5; ++i) alert( i );//1-4
-
-
-//Виведіть парні числа
-for (let mumber = 2; number <= 10; number++) {
-    if (mumber % 2 == 0) {
-        alert(mumber);
-    }
+//Перепишіть "switch" в аналогічну з використанням "if"
+  if (browser === "Edge") {
+    alert("You've got the Edge!");
+} else if (browser === "Chrome" || 
+           browser === "Firefox" || 
+           browser === "Safari" || 
+           browser === "Opera") {
+    alert("Ми підтримуємо і ці браузери");
+} else {
+    alert("Маємо надію, що ця сторінка виглядає добре!");
 }
 
 
-//Замініть цикл "for" на "while"
-for (let i = 0; i < 3; i++) {
-    alert( `число ${i}!` );
-  }
-//рішення:
-let i = 0;
-do {
-  alert(i);
-  i++;
-} while (i < 3);
+//Перепишіть умови "if" в конструкцію "switch"
+let a = prompt("a?", "")
 
-
-//Повторяти цикл, доки ввід невірний
-let number;
-do {
-    number = prompt("Write num > 100", 0)
-} while (number <= 100 && number);
-
-
-//Вивести прості числа
-let n = 10;
-for (let i = 2; i <= n; i++) {
-  let isPrime = true;
-
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0) {
-      isPrime = false;
+  switch (a) {
+    case "0":
+      alert("0");
       break;
-    }
-  }
 
-  if (isPrime) {
-    alert(i);
-  }
+    case "1":
+      alert("1");
+      break;
+    
+    case "2":
+    case "3":
+      alert("2,3");
+      break;
 }
+
