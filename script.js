2dcdf9cff40ddbac6ab5d88a3643600222634039
Your Name <you@example.com>
2024-10-12 12:53:36 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-5 > 4 //true
-"ананас" > "яблуко" //false
-"2" > "12" //true
-undefined == null //true
-undefined === null //false
-null == "\n0\n" //false
-null === +"\n0\n" //false
 
+//if (рядок з нулем)
+if ("0") { 
+    alert( 'Привіт' );
+  } //Так, alert спрацює в цьому випадку
+
+
+//Назва JavaScript
+let company = prompt("Яка офіційна назва JavaScript?");
+
+    if (company === "ECMAScript") {
+        alert("Правильно!");
+    } else {
+        alert("Ви не знаєте? ECMAScript!");
+    }
+
+
+//Покажіть знак
+let number = Number(prompt("Введіть будь-яке число"));
+    if (number > 0) {
+        alert(1)
+    } else if (number < 0) {
+        alert(-1)
+    } else {
+        alert(0)
+    }
+
+
+// Перепишіть 'if' на '?'
+let result;
+    result = (a + b < 4) ? 'Нижче' : 'Вище';
+
+
+//Перепишіть 'if..else' на '?'
+let message =
+    (login === 'Працівник') ? 'Привіт':
+    (login === 'Директор') ? 'Вітаю':
+    (login === '') ? 'Немає логіну':
+    '';
