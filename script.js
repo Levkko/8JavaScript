0f9f12959661732fa0638df09ecaf5cab0ce4986
Your Name <you@example.com>
2024-10-17 21:21:14 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-//Перепишіть "switch" в аналогічну з використанням "if"
-  if (browser === "Edge") {
-    alert("You've got the Edge!");
-} else if (browser === "Chrome" || 
-           browser === "Firefox" || 
-           browser === "Safari" || 
-           browser === "Opera") {
-    alert("Ми підтримуємо і ці браузери");
-} else {
-    alert("Маємо надію, що ця сторінка виглядає добре!");
+//Перепишіть функцію, використовуючи '?' або '||'
+  function checkAge(age) {
+    return (age > 18) ? true : confirm('Батьки дозволили?');
+  }
+
+  function checkAge(age) {
+    return (age > 18) || confirm('Батьки дозволили?');
+  }
+
+
+//Функція min(a, b)
+function minLetter(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
 
-//Перепишіть умови "if" в конструкцію "switch"
-let a = prompt("a?", "")
-
-  switch (a) {
-    case "0":
-      alert("0");
-      break;
-
-    case "1":
-      alert("1");
-      break;
-    
-    case "2":
-    case "3":
-      alert("2,3");
-      break;
-}
-
+//Функція pow(x, n)
+function pow(x, n) {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  let x = prompt("x?", '');
+  let n = prompt("n?", '');
+  
+  if (n < 1) {
+    alert(`Степінь ${n} не підтримується, використовуйте натуральне число`);
+  } else {
+    alert( pow(x, n) );
+  }
+//Я це так і не зміг зробити, то дуже складне логічне завдання :/
