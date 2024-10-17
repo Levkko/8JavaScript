87ec9d2b133de6e893ef69cfbaaf10e0d7da4120
Your Name <you@example.com>
2024-10-17 23:08:18 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-//Перепишіть функцію, використовуючи '?' або '||'
-  function checkAge(age) {
-    return (age > 18) ? true : confirm('Батьки дозволили?');
-  }
+//Перепишіть з використанням стрілкових функцій
+let ask = (question, yes, no) => 
+    confirm(question) ? yes() : no();
 
-  function checkAge(age) {
-    return (age > 18) || confirm('Батьки дозволили?');
-  }
-
-
-//Функція min(a, b)
-function minLetter(a, b) {
-    if (a < b) {
-        return a;
-    } else {
-        return b;
-    }
-}
-
-
-//Функція pow(x, n)
-function pow(x, n) {
-    let result = x;
-  
-    for (let i = 1; i < n; i++) {
-      result *= x;
-    }
-  
-    return result;
-  }
-  
-  let x = prompt("x?", '');
-  let n = prompt("n?", '');
-  
-  if (n < 1) {
-    alert(`Степінь ${n} не підтримується, використовуйте натуральне число`);
-  } else {
-    alert( pow(x, n) );
-  }
-//Я це так і не зміг зробити, то дуже складне логічне завдання :/
+ask(
+    "Ви згодні?",
+    () => alert("Ви погодились."),
+    () => alert("Ви скасували виконання.") 
+  )
